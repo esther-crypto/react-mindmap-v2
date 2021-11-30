0493278149427c4cacfa3df37c2a884934d15457
esther-crypto <estherwambuiirungu@gmail.com>
2021-11-30 11:37:03 +0300

    Updates

---
+++ precache-manifest.360a9ada26524a35f687cc9788ae508f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060b201745c64304f7252952394a3eb9",
+    "url": "/react-mindmap-v2/index.html"
+  },
+  {
+    "revision": "8a494ae6ff709e3a56a9",
+    "url": "/react-mindmap-v2/static/css/chunk-blinkmind.bb4fe98d.chunk.css"
+  },
+  {
+    "revision": "01032a6908c22352fedc",
+    "url": "/react-mindmap-v2/static/css/chunk-blueprint.2b77d060.chunk.css"
+  },
+  {
+    "revision": "8a494ae6ff709e3a56a9",
+    "url": "/react-mindmap-v2/static/js/chunk-blinkmind.40bfdff5.chunk.js"
+  },
+  {
+    "revision": "01032a6908c22352fedc",
+    "url": "/react-mindmap-v2/static/js/chunk-blueprint.14663b61.chunk.js"
+  },
+  {
+    "revision": "37f2dd49324c4c59842c",
+    "url": "/react-mindmap-v2/static/js/chunk-libs.fe35c789.chunk.js"
+  },
+  {
+    "revision": "6401870bdfc3922c6ee5",
+    "url": "/react-mindmap-v2/static/js/chunk-slate.2e7946d7.chunk.js"
+  },
+  {
+    "revision": "ea10da298b55c47cffe2",
+    "url": "/react-mindmap-v2/static/js/main.2587a0d8.chunk.js"
+  },
+  {
+    "revision": "7d331d8b2667bc969c45",
+    "url": "/react-mindmap-v2/static/js/runtime~main.72237ae2.js"
+  },
+  {
+    "revision": "4f8c49830f9d89080c0e14ef27fe5119",
+    "url": "/react-mindmap-v2/static/media/default.4f8c4983.jpg"
+  },
+  {
+    "revision": "1615fe237931cc9888e8da84ee2bddf9",
+    "url": "/react-mindmap-v2/static/media/iconfont.1615fe23.ttf"
+  },
+  {
+    "revision": "2ec140ab2784852eaa0acd4969f75c25",
+    "url": "/react-mindmap-v2/static/media/iconfont.2ec140ab.svg"
+  },
+  {
+    "revision": "46a2ccc0f9b5e741b0a7236a45595cfc",
+    "url": "/react-mindmap-v2/static/media/iconfont.46a2ccc0.svg"
+  },
+  {
+    "revision": "52b6ee751b76bedccfefc9862ac73b8c",
+    "url": "/react-mindmap-v2/static/media/iconfont.52b6ee75.woff"
+  },
+  {
+    "revision": "5f6ea17778af9df7f32a0604f37db8ac",
+    "url": "/react-mindmap-v2/static/media/iconfont.5f6ea177.eot"
+  },
+  {
+    "revision": "aa15821e33931d66ea8a44e325c97faf",
+    "url": "/react-mindmap-v2/static/media/iconfont.aa15821e.eot"
+  },
+  {
+    "revision": "e33c2f4f3e7fb9f3136604575d625c1a",
+    "url": "/react-mindmap-v2/static/media/iconfont.e33c2f4f.woff"
+  },
+  {
+    "revision": "e740c559e16c78be96e7c29125d2c683",
+    "url": "/react-mindmap-v2/static/media/iconfont.e740c559.ttf"
+  },
+  {
+    "revision": "2f59f00b1baa3f74747694eff9e53875",
+    "url": "/react-mindmap-v2/static/media/theme1.2f59f00b.jpg"
+  },
+  {
+    "revision": "b18193fbba175e35f6fbf3391f385723",
+    "url": "/react-mindmap-v2/static/media/theme2.b18193fb.jpg"
+  },
+  {
+    "revision": "61c97910ac792963391befcf87c7ce00",
+    "url": "/react-mindmap-v2/static/media/theme3.61c97910.jpg"
+  },
+  {
+    "revision": "1e21408c23fc4c6933552278be804add",
+    "url": "/react-mindmap-v2/static/media/theme4.1e21408c.jpg"
+  }
+]);
---
+++ static/js/main.2587a0d8.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonpreact-mindmap"]=window["webpackJsonpreact-mindmap"]||[]).push([[4],{350:function(n,p,a){n.exports=a(879)}},[[350,5,0,1,3,2]]]);
+//# sourceMappingURL=main.2587a0d8.chunk.js.map
